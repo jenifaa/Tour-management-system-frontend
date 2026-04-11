0787ce843bc1ba21baa562022e21514d30a7e258
jenifaa <jefrahman32@gmail.com>
2026-04-11 06:16:46 +0600

    created project and setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
+import { Outlet } from "react-router";
+import CommonLayout from "./components/layout/CommonLayout";
+
 function App() {
   return (
     <>
-      <h2>Hello world</h2>
+      <CommonLayout>
+        <Outlet></Outlet>
+      </CommonLayout>
     </>
   );
 }
