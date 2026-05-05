9e4c0f7f517c49e78677fb6b7d0d2c7fe6bedb0a
jenifaa <jefrahman32@gmail.com>
2026-05-05 23:56:59 +0600

    added table for tour type

--- src/pages/admin/AddTourType.tsx
+++ src/pages/admin/AddTourType.tsx
@@ -1,10 +1,40 @@
 import { useGetTourTypesQuery } from "@/redux/features/Tour/tour.api";
-
+import {
+  Table,
+  TableBody,
+  TableCaption,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
 function AddTourType() {
   const { data } = useGetTourTypesQuery(undefined);
   console.log(data);
 
-  return <div>AddTourType</div>;
+  return (
+    <div>
+      <Table>
+        <TableCaption>A list of your recent invoices.</TableCaption>
+        <TableHeader>
+          <TableRow>
+            <TableHead className="w-25">Invoice</TableHead>
+            <TableHead>Status</TableHead>
+            <TableHead>Method</TableHead>
+            <TableHead className="text-right">Amount</TableHead>
+          </TableRow>
+        </TableHeader>
+        <TableBody>
+          <TableRow>
+            <TableCell className="font-medium">INV001</TableCell>
+            <TableCell>Paid</TableCell>
+            <TableCell>Credit Card</TableCell>
+            <TableCell className="text-right">$250.00</TableCell>
+          </TableRow>
+        </TableBody>
+      </Table>
+    </div>
+  );
 }
 
 export default AddTourType;
