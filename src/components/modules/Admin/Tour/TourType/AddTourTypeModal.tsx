e9ba3a8e674a370e7dbc5ca658e9dd3fe5a046a5
jenifaa <jefrahman32@gmail.com>
2026-05-06 00:26:37 +0600

    created add tour form modal

---
+++ src/components/modules/Admin/Tour/TourType/AddTourTypeModal.tsx
@@ -0,0 +1,50 @@
+import { Button } from "@/components/ui/button";
+import {
+  Dialog,
+  DialogClose,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import { Field, FieldGroup } from "@/components/ui/field";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+
+export function AddTourTypeModal() {
+  return (
+    <Dialog>
+      <form>
+        <DialogTrigger >
+          <Button variant="outline">Open Dialog</Button>
+        </DialogTrigger>
+        <DialogContent className="sm:max-w-sm">
+          <DialogHeader>
+            <DialogTitle>Add Tour Type</DialogTitle>
+            <DialogDescription>
+              Enter the details for the new tour type.
+            </DialogDescription>
+          </DialogHeader>
+          <FieldGroup>
+            <Field>
+              <Label htmlFor="name-1">Name</Label>
+              <Input id="name-1" name="name" defaultValue="Pedro Duarte" />
+            </Field>
+            <Field>
+              <Label htmlFor="username-1">Username</Label>
+              <Input id="username-1" name="username" defaultValue="@peduarte" />
+            </Field>
+          </FieldGroup>
+          <DialogFooter>
+            <DialogClose>
+              <Button variant="outline">Cancel</Button>
+            </DialogClose>
+            <Button type="submit">Save changes</Button>
+          </DialogFooter>
+        </DialogContent>
+      </form>
+    </Dialog>
+  );
+}
--- src/pages/admin/AddTourType.tsx
+++ src/pages/admin/AddTourType.tsx
@@ -2,37 +2,51 @@ import { useGetTourTypesQuery } from "@/redux/features/Tour/tour.api";
 import {
   Table,
   TableBody,
-  TableCaption,
+  // TableCaption,
   TableCell,
   TableHead,
   TableHeader,
   TableRow,
 } from "@/components/ui/table";
+import { Button } from "@base-ui/react";
+import { Trash2 } from "lucide-react";
+import { AddTourTypeModal } from "@/components/modules/Admin/Tour/TourType/AddTourTypeModal";
 function AddTourType() {
   const { data } = useGetTourTypesQuery(undefined);
   console.log(data);
 
   return (
     <div className="w-full max-w-7xl mx-auto px-5">
-      <Table>
-        <TableCaption>A list of your recent invoices.</TableCaption>
-        <TableHeader>
-          <TableRow>
-            <TableHead className="w-25">Invoice</TableHead>
-            <TableHead>Status</TableHead>
-            <TableHead>Method</TableHead>
-            <TableHead className="text-right">Amount</TableHead>
-          </TableRow>
-        </TableHeader>
-        <TableBody>
-          <TableRow>
-            <TableCell className="font-medium">INV001</TableCell>
-            <TableCell>Paid</TableCell>
-            <TableCell>Credit Card</TableCell>
-            <TableCell className="text-right">$250.00</TableCell>
-          </TableRow>
-        </TableBody>
-      </Table>
+      <div className="flex justify-between my-10">
+        <h1 className="text-xl font-semibold">Tour Types</h1>
+        <AddTourTypeModal></AddTourTypeModal>
+      </div>
+      <div className="border border-muted rounded-md">
+        {" "}
+        <Table>
+          {/* <TableCaption>A list of your recent invoices.</TableCaption> */}
+          <TableHeader>
+            <TableRow>
+              <TableHead className="w-25">Name</TableHead>
+              <TableHead>Action</TableHead>
+            </TableRow>
+          </TableHeader>
+          <TableBody>
+            {data?.map((item: { name: string }) => (
+              <TableRow>
+                <TableCell className="font-medium w-full">
+                  {item?.name}
+                </TableCell>
+                <TableCell>
+                  <Button>
+                    <Trash2 className="w-4 h-4" />
+                  </Button>
+                </TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </div>
     </div>
   );
 }
